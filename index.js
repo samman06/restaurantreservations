f81e46cf495685f65346b1f2bf2c359ff5b5bfe2
Mohammed-Elsamman <monem7242@gmail.com>
2020-02-25 18:48:32 +0200

    create config file has uri of database connection and create a connection with the database

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 4000;
 const cors = require('cors');
-
+const keys = require('./configs/keys');
+const uri = keys.mongoURI;
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(expressValidator());
 
+//start connection to database
+mongoose.Promise = global.Promise;
+mongoose.set('useCreateIndex', true);
+mongoose.connect(uri, {
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+    useNewUrlParser: true
+}, err => {
+    if (!err) console.log("started mongodb connection");
+    else console.log(err);
+
+});
 
 
 
