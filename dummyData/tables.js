98382c15dd955212429550d4f26bb73cf0da0c96
Mohammed-Elsamman <monem7242@gmail.com>
2020-02-25 20:40:09 +0200

    create dummyData folder and create dummy data for tables

---
+++ dummyData/tables.js
@@ -0,0 +1,16 @@
+const dummyTables =()=> [
+    {tableNumber: 1, numberOfPerson: 2},
+    {tableNumber: 2, numberOfPerson: 2},
+    {tableNumber: 3, numberOfPerson: 2},
+    {tableNumber: 4, numberOfPerson: 2},
+    {tableNumber: 5, numberOfPerson: 5},
+    {tableNumber: 6, numberOfPerson: 5},
+    {tableNumber: 7, numberOfPerson: 5},
+    {tableNumber: 8, numberOfPerson: 5},
+    {tableNumber: 9, numberOfPerson: 5},
+    {tableNumber: 10, numberOfPerson: 5},
+    {tableNumber: 11, numberOfPerson: 5},
+    {tableNumber: 12, numberOfPerson: 10},
+    {tableNumber: 13, numberOfPerson: 10},
+];
+module.exports = dummyTables;
