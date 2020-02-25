df9a54b7d64a74f4b12a1b1ed23528d8829d9267
Mohammed-Elsamman <monem7242@gmail.com>
2020-02-25 21:00:13 +0200

    create function to insert dummy data of 13 tables if there is no tables

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app = express();
 const expressValidator = require('express-validator');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
-const tableRouter = require('./routes/table');
-
-const PORT = process.env.PORT || 4000;
+const mongoose = require('mongoose');
 const cors = require('cors');
+const tableRouter = require('./routes/table');
+const TableModel = require('./models/table');
+const dummyTables = require('./dummyData/tables')();
+const PORT = process.env.PORT || 4000;
 const keys = require('./configs/keys');
-const uri = keys.mongoURI;
 app.use(cors());
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(expressValidator());
 
 //start connection to database
+const uri = keys.mongoURI;
 mongoose.Promise = global.Promise;
 mongoose.set('useCreateIndex', true);
 mongoose.connect(uri, {
@@ -30,7 +31,16 @@ mongoose.connect(uri, {
 
 app.use('/table', tableRouter);
 
+const insertDummyTables = async () => {
+    const data = await TableModel.find();
+    if (data.length !== 0) return;
+    await dummyTables.map(table =>{
+        const newTable = new TableModel(table);
+        newTable.save()
+    });
+};
 
+insertDummyTables();
 
 app.listen(PORT, (req, res) => {
     console.log("server running on port: " + PORT);
