c0e59868c66bc06a2e6b6bf23a3107dff718880c
Mohammed-Elsamman <monem7242@gmail.com>
2020-02-26 18:26:19 +0200

    create client directory react application and install classnames, axios and bootstrap

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+        <div>restaurant reservation</div>
+        );
+    }
+}
+
+export default App;
