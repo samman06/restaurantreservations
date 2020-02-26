51e0b8e084f85793d290854841853b35aaae8b3d
Mohammed-Elsamman <monem7242@gmail.com>
2020-02-26 16:40:35 +0200

    create function to test the route of get avaolable tables

--- test/testTable.js
+++ test/testTable.js
@@ -5,6 +5,7 @@ chai.use(chaiHttp);
 const app = require('../index');
 describe("testing on table module", () => {
 
+
     describe("GET /table", () => {
         it("should get all tables", (done) => {
             chai.request(app)
@@ -16,6 +17,19 @@ describe("testing on table module", () => {
                 });
         });
     });
+
+    describe("GET /table/available", () => {
+        it("should get all available tables", (done) => {
+            chai.request(app)
+                .get('/table/available')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('Array');
+                    done();
+                });
+        });
+    });
+
 
 
 });
