52b8f08769e1fd5e58d10b7af57fc779e71ca046
Mohammed-Elsamman <monem7242@gmail.com>
2020-02-26 16:53:14 +0200

    test the route of add new table with 3 scenarios

--- test/testTable.js
+++ test/testTable.js
@@ -30,6 +30,45 @@ describe("testing on table module", () => {
         });
     });
 
+    describe("POST /table", () => {
+        it("should add new table", (done) => {
+            chai.request(app)
+                .post('/table')
+                .send({tableNumber: "5532", numberOfPerson: "3"})
+                .end(res => {
+                    res.should.have.status(201);
+                    res.body.should.be.a("Object");
+                    res.body.message.should.contains("The table is Created successfully");
+                    done();
+                })
+        });
+
+        it("should not add new table because the table is already exists", (done) => {
+            chai.request(app)
+                .post('/table')
+                .send({tableNumber: "5532", numberOfPerson: "3"})
+                .end(res => {
+                    res.should.have.status(201);
+                    res.body.should.be.a("Object");
+                    res.body.errors.tableNumber.should.contains("The table is already exists");
+                    done();
+                })
+        });
+
+        it("should not add new table because the data is not valid", (done) => {
+            chai.request(app)
+                .post('/table')
+                .send({tableNumber: "31W", numberOfPerson: "3W"})
+                .end(res => {
+                    res.should.have.status(200);
+                    res.body.errors.numberOfPerson.should.contains("number of person should be number");
+                    res.body.errors.tableNumber.should.contains("the number of table should be number");
+                    done();
+                })
+        });
+    });
+
+
 
 
 });
