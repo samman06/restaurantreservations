2801d311cbdc22dc92797a3d1974e153451d6a88
Mohammed-Elsamman <monem7242@gmail.com>
2020-02-27 02:23:29 +0200

    prepare to deploy on heroku

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const tableRouter = require('./routes/table');
+const path = require("path");
 const TableModel = require('./models/table');
 const dummyTables = require('./dummyData/tables')();
+const tableRouter = require('./routes/table');
 const reservationRouter = require('./routes/reservation');
 const PORT = process.env.PORT || 4000;
 const keys = require('./configs/keys');
@@ -34,16 +35,23 @@ mongoose.connect(uri, {
 app.use('/table', tableRouter);
 // reservations router
 app.use('/reservation', reservationRouter);
+//server static assets if in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build'))
+    })
+}
+
 const insertDummyTables = async () => {
     const data = await TableModel.find();
     if (data.length !== 0) return;
-    await dummyTables.map(table =>{
+    await dummyTables.map(table => {
         const newTable = new TableModel(table);
         newTable.save()
     });
 };
-
-insertDummyTables();
+insertDummyTables().then();
 
 app.listen(PORT, (req, res) => {
     console.log("server running on port: " + PORT);
