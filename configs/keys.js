3063d8fe2243f462bfc0592409342d59d006f4ff
samman06 <monem7242@gmail.com>
2021-09-30 17:02:36 +0200

    add production DB

--- configs/keys.js
+++ configs/keys.js
@@ -1,4 +1,4 @@
-module.exports={
-    mongoURI : "mongodb://mongo:27017/restaurant",
+module.exports = {
+    mongoURI: process.env.DB || "mongodb://mongo:27017/restaurant",
     // mongoURI : "mongodb+srv://monem:monem0006@cluster0-2sq7q.mongodb.net/test?retryWrites=true&w=majority",
 }
